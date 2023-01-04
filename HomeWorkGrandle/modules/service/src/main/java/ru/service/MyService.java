f9f8259ff55a6941ddef7f9b160e6643ec1346da
mneliseev <mneliseev@gmail.com>
2023-01-04 22:17:37 +0500

    Init project

---
+++ HomeWorkGrandle/modules/service/src/main/java/ru/service/MyService.java
@@ -0,0 +1,28 @@
+package ru.service;
+
+import ru.db.Db;
+import ru.db.DbSetting;
+import ru.db.MyEntity;
+
+import java.util.UUID;
+
+public class MyService {
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
---
+++ HomeWorkMaven/modules/api/src/main/java/ru/api/Main.java
@@ -0,0 +1,14 @@
+package ru.api;
+
+import ru.db.MyEntity;
+import ru.service.MyService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
